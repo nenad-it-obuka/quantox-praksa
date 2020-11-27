6c1e142f7497586d675f09b06d5c79bf33fe2099
itnenad <ndicic@gmail.com>
2020-11-27 11:52:01 +0100

    modified

--- src/Form.js
+++ src/Form.js
@@ -1,35 +1,67 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
 
 
+class Form extends React.Component {  
+  constructor(props) {
+    super(props);  
+    
+    this.state = {
+      fname: '',
+      lname: '',
+      email: '',
+      company: '',
+      csize: '',
+      phone: '',
+      shouldShowConfirmation: false
+    };
 
+    this.handleChange = this.handleChange.bind(this);    
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  
+  handleChange (event) {        
+    let id = event.target.id;
+    let value = event.target.value;
 
-class Form extends React.Component {
-    render() {
-        return (
-            <form>        
-                <span id="title">Request a demo</span><br/><br/>            
-                <label for="fname">FIRST NAME: <span class="star">*</span></label>
-                <input class="fields" type="text" id="fname" required/><br/>
-                <label for="lname">LAST NAME: <span class="star">*</span></label>
-                <input class="fields" type="text" id="lname" required/><br/>
-                <label for="email">WORK EMAIL: <span class="star">*</span></label>
-                <input class="fields" type="email" id="email" required/><br/>
-                <label for="company">COMPANY: <span class="star">*</span></label>
-                <input class="fields" type="text" id="company" required/><br/>
-                <label for="csize">COMPANY SIZE: <span class="star">*</span></label>
-                <select class="fields" id="csize" required>
-                    <option></option>
-                    <option value="small">1-100</option>
-                    <option value="medium">101-1000</option>
-                    <option value="large">1000+</option>
-                </select><br/>
-                <label for="phone">PHONE NUMBER: <span class="star">*</span></label>
-                <input class="fields" type="tel" id="phone" required/><br/>
-                <input id="submit-button" type="submit" value="SUBMIT"/>
-            </form>
-        );
+    this.setState({[id]: value});
+  }
+  
+  handleSubmit (event) {            
+    event.preventDefault();
+    
+    this.setState({shouldShowConfirmation: true});          
+  }
+
+  render() {
+    if (this.state.shouldShowConfirmation) {          
+      return <Redirect to="/confirmation" />;
+    } else {
+      return (          
+        <form onSubmit={this.handleSubmit}>
+          <span id="title">Request a demo</span><br/><br/>            
+          <label htmlFor="fname">FIRST NAME: <span className="star">*</span></label>
+          <input className="fields" id="fname" type="text" onChange={this.handleChange} required/><br/>
+          <label htmlFor="lname">LAST NAME: <span className="star">*</span></label>
+          <input className="fields" id="lname" type="text" onChange={this.handleChange} required/><br/>
+          <label htmlFor="email">WORK EMAIL: <span className="star">*</span></label>
+          <input className="fields" id="email" type="email" onChange={this.handleChange} required/><br/>
+          <label htmlFor="company">COMPANY: <span className="star">*</span></label>
+          <input className="fields" id="company" type="text" onChange={this.handleChange} required/><br/>
+          <label htmlFor="csize">COMPANY SIZE: <span className="star">*</span></label>
+          <select className="fields" id="csize" onChange={this.handleChange} required>
+            <option></option>
+            <option value="small">1-100</option>
+            <option value="medium">101-1000</option>
+            <option value="large">1000+</option>
+          </select><br/>
+          <label htmlFor="phone">PHONE NUMBER: <span className="star">*</span></label>
+          <input className="fields" id="phone" type="tel" onChange={this.handleChange} required/><br/>
+          <input id="submit-button" type="submit" value="SUBMIT"/>
+        </form>
+      );
     }
-}
-
+  } 
+}   
 
 export default Form;
--- src/pages/Confirmation.js
+++ src/pages/Confirmation.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 
 
 
 class Confirmation extends React.Component {
     render() {
-        return (
-            <div>This is Confirmation page</div> 
+        return (            
+            <div>
+                <p>                        
+                    Thank you. Your data is submitted.<br/><br/>
+                    This is Confirmation page.<br/><br/>
+                    <Link to="/">Go back to Home page</Link>
+                </p>
+            </div>             
         );
     }
 }
